b6fa7cbc350c7d4d3d55d857150f2306786b50d5
knowhw <142631271+knowhw@users.noreply.github.com>
2024-03-22 09:07:38 -0500

    Add files via upload

---
+++ scatter.py
@@ -0,0 +1,36 @@
+
+
+workdir="/media/linux/WORKDIR"
+
+
+import os
+path = os.path.join(workdir, "data.csv")
+
+import pyautogui
+import time
+
+exists = None if not os.path.exists(path) else 1.
+
+
+
+
+def csv_4scatter_w(x,y):
+	
+	
+	with open (path, mode="a+") as data:
+		data.seek(1)
+		
+			
+		if not exists:
+			data.write("x,y\n")
+
+		else:
+			if "%s,%s\n" % (x,y) != data.readlines() [-1]: data.write("%s,%s\n" % (x,y))
+			
+while 1:
+	
+	x, y = pyautogui.position()
+	# mouse imlecinin ekrandaki konumu
+	
+	csv_4scatter_w(x,y)
+	time.sleep(.3)
