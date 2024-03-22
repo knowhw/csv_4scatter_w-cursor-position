a8b004bc977cc8bf2b2bdae4dc49f71fa5af9544
knowhw <142631271+knowhw@users.noreply.github.com>
2024-03-22 14:28:09 -0500

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -10,24 +10,31 @@ import pyautogui
 import time
 
 
+exists = None if not os.path.exists(path) else 1.
+
 def csv_4scatter_w(x,y):
-	exists = None if not os.path.exists(path) else 1.
+	
 	
 	with open (path, mode="a+") as data:
 		data.seek(1)
 		
-			
 		if not exists:
 			data.write("x,y\n")
 
 		else:
-			if "%s,%s\n" % (x,y) != data.readlines() [-1]: data.write("%s,%s\n" % (x,y))
 			
-
-while 1.:
+			position= "%s,%s\n" % (x,y)
+			last_index = position != data.readlines() [-1]
+			
+			if last_index: 
+				data.write(position)
+				print(position.strip())
+			
+			
+while 1:
 	
 	x, y = pyautogui.position()
 	# mouse imlecinin ekrandaki konumu
 	
 	csv_4scatter_w(x,y)
-	time.sleep(.3)
+	time.sleep(.1)
